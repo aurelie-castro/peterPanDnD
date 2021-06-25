c957370055c098c7d812abe4f2ddb745cd75fc61
aurelie-castro <acdemarly@gmail.com>
2021-06-25 11:13:30 +0200

    added unmute func

--- js/game.js
+++ js/game.js
@@ -24,12 +24,12 @@ var nextArrow;
 let successfulDropoff;
 
 var holdSound;
-
 var wrongSound;
-
 var correctSound;
-
 var finishSound;
+
+var soundButton;
+var hasBeenClicked;
 
 var star;
 var starScale;
@@ -56,6 +56,9 @@ function preload() {
     this.load.audio('wrong', './assets/wrong.wav');
     this.load.audio('correct', './assets/correct.wav');
     this.load.audio('finish', './assets/finish.wav');
+    
+    //---sound button----
+    this.load.image('soundBtn', './assets/volume-up (1).png');
     
     //---star at the end---
     this.load.image('star', './assets/purple-star.png');
@@ -86,6 +89,13 @@ function create() {
     wrongSound = this.sound.add('wrong');
     correctSound = this.sound.add('correct');
     finishSound = this.sound.add('finish');
+    
+    //----audio  btn----
+    soundButton = this.add.image(50,50, 'soundBtn');
+    soundButton.setScale(0.1);
+    soundButton.setInteractive();
+    soundButton.alpha = 0.5;
+    soundButton.on('pointerdown', enableMusic);
     
     //----les membres-----
     var head = this.add.image(50, 380, 'head', Phaser.Math.RND.pick(frames)).setInteractive();
@@ -229,9 +239,16 @@ function update() {
         if (starScale > 0.2){
             starScale = 0.2;
         } }
+    
+       if (hasBeenClicked === true){
+        soundButton.alpha = 1;
+        }
 }
 function onClick(){
 //    window.open("https://www.google.com", "_blank");
     window.location.replace("http://www.w3schools.com");
 
+}
+function enableMusic(){
+    hasBeenClicked = true;
 }
